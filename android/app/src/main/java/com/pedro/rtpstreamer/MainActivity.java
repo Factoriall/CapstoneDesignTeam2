2696811d7c67549a5f08505f8f277dffd8b98dd5
tae6ae <tae6ae@gmail.com>
2021-10-03 00:56:15 +0900

    Modify MainActivity

--- android/app/src/main/java/com/pedro/rtpstreamer/MainActivity.java
+++ android/app/src/main/java/com/pedro/rtpstreamer/MainActivity.java
@@ -25,39 +25,21 @@ import android.os.Bundle;
 import androidx.core.app.ActivityCompat;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
+
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.pedro.rtpstreamer.backgroundexample.BackgroundActivity;
-import com.pedro.rtpstreamer.customexample.RtmpActivity;
-import com.pedro.rtpstreamer.customexample.RtspActivity;
 import com.pedro.rtpstreamer.defaultexample.ExampleRtmpActivity;
-import com.pedro.rtpstreamer.defaultexample.ExampleRtspActivity;
-import com.pedro.rtpstreamer.displayexample.DisplayActivity;
-import com.pedro.rtpstreamer.filestreamexample.RtmpFromFileActivity;
-import com.pedro.rtpstreamer.filestreamexample.RtspFromFileActivity;
-import com.pedro.rtpstreamer.openglexample.OpenGlRtmpActivity;
-import com.pedro.rtpstreamer.openglexample.OpenGlRtspActivity;
-import com.pedro.rtpstreamer.surfacemodeexample.SurfaceModeRtmpActivity;
-import com.pedro.rtpstreamer.surfacemodeexample.SurfaceModeRtspActivity;
-import com.pedro.rtpstreamer.texturemodeexample.TextureModeRtmpActivity;
-import com.pedro.rtpstreamer.texturemodeexample.TextureModeRtspActivity;
 import com.pedro.rtpstreamer.utils.ActivityLink;
-import com.pedro.rtpstreamer.utils.ImageAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static android.os.Build.VERSION_CODES.JELLY_BEAN;
 import static android.os.Build.VERSION_CODES.JELLY_BEAN_MR2;
 import static android.os.Build.VERSION_CODES.LOLLIPOP;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-  private GridView list;
-  private List<ActivityLink> activities;
+  private ActivityLink link;
 
   private final String[] PERMISSIONS = {
       Manifest.permission.RECORD_AUDIO, Manifest.permission.CAMERA,
@@ -72,67 +54,33 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     TextView tvVersion = findViewById(R.id.tv_version);
     tvVersion.setText(getString(R.string.version, BuildConfig.VERSION_NAME));
 
-    list = findViewById(R.id.list);
-    createList();
-    setListAdapter(activities);
+    Button button = findViewById(R.id.b_rtmp);
+    button.setOnClickListener(this);
 
     if (!hasPermissions(this, PERMISSIONS)) {
       ActivityCompat.requestPermissions(this, PERMISSIONS, 1);
     }
   }
 
-  private void createList() {
-    activities = new ArrayList<>();
-    activities.add(
-        new ActivityLink(new Intent(this, RtmpActivity.class), getString(R.string.rtmp_streamer),
-            JELLY_BEAN));
-    activities.add(
-        new ActivityLink(new Intent(this, RtspActivity.class), getString(R.string.rtsp_streamer),
-            JELLY_BEAN));
-    activities.add(new ActivityLink(new Intent(this, ExampleRtmpActivity.class),
-        getString(R.string.default_rtmp), JELLY_BEAN));
-    activities.add(new ActivityLink(new Intent(this, ExampleRtspActivity.class),
-        getString(R.string.default_rtsp), JELLY_BEAN));
-    activities.add(new ActivityLink(new Intent(this, RtmpFromFileActivity.class),
-        getString(R.string.from_file_rtmp), JELLY_BEAN_MR2));
-    activities.add(new ActivityLink(new Intent(this, RtspFromFileActivity.class),
-        getString(R.string.from_file_rtsp), JELLY_BEAN_MR2));
-    activities.add(new ActivityLink(new Intent(this, SurfaceModeRtmpActivity.class),
-        getString(R.string.surface_mode_rtmp), LOLLIPOP));
-    activities.add(new ActivityLink(new Intent(this, SurfaceModeRtspActivity.class),
-        getString(R.string.surface_mode_rtsp), LOLLIPOP));
-    activities.add(new ActivityLink(new Intent(this, TextureModeRtmpActivity.class),
-        getString(R.string.texture_mode_rtmp), LOLLIPOP));
-    activities.add(new ActivityLink(new Intent(this, TextureModeRtspActivity.class),
-        getString(R.string.texture_mode_rtsp), LOLLIPOP));
-    activities.add(new ActivityLink(new Intent(this, OpenGlRtmpActivity.class),
-        getString(R.string.opengl_rtmp), JELLY_BEAN_MR2));
-    activities.add(new ActivityLink(new Intent(this, OpenGlRtspActivity.class),
-        getString(R.string.opengl_rtsp), JELLY_BEAN_MR2));
-    activities.add(new ActivityLink(new Intent(this, DisplayActivity.class),
-        getString(R.string.display_rtmp), LOLLIPOP));
-    activities.add(new ActivityLink(new Intent(this, BackgroundActivity.class),
-        getString(R.string.service_rtp), LOLLIPOP));
-  }
-
-  private void setListAdapter(List<ActivityLink> activities) {
-    list.setAdapter(new ImageAdapter(activities));
-    list.setOnItemClickListener(this);
-  }
-
-  @Override
-  public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-    if (hasPermissions(this, PERMISSIONS)) {
-      ActivityLink link = activities.get(i);
-      int minSdk = link.getMinSdk();
-      if (Build.VERSION.SDK_INT >= minSdk) {
-        startActivity(link.getIntent());
-        overridePendingTransition(R.transition.slide_in, R.transition.slide_out);
-      } else {
-        showMinSdkError(minSdk);
-      }
-    } else {
-      showPermissionsErrorAndRequest();
+  public void onClick(View view) {
+    switch (view.getId()) {
+      case R.id.b_rtmp:
+        link = new ActivityLink(new Intent(this, ExampleRtmpActivity.class),
+                getString(R.string.default_rtmp), JELLY_BEAN);
+        if (hasPermissions(this, PERMISSIONS)) {
+          int minSdk = link.getMinSdk();
+          if (Build.VERSION.SDK_INT >= minSdk) {
+            startActivity(link.getIntent());
+            overridePendingTransition(R.transition.slide_in, R.transition.slide_out);
+          } else {
+            showMinSdkError(minSdk);
+          }
+        } else {
+          showPermissionsErrorAndRequest();
+        }
+        break;
+      default:
+        break;
     }
   }
 
